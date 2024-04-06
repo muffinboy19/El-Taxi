4d4108e47ff46f6c73cce7979a6c422cd121a125
Ishant Kumawat <120731858+insanecoder02@users.noreply.github.com>
2024-04-07 01:16:25 +0530

    commit 1 auth page

--- app/src/main/java/com/example/el_taxi/MainActivity.kt
+++ app/src/main/java/com/example/el_taxi/MainActivity.kt
@@ -8,5 +8,6 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        window.statusBarColor = 0xFF000000.toInt()
     }
 }
